c1209327cea2984a61a63e1e9e3bd1822c4a8d83
Nikhil Gupta <67306052+nikhilg153@users.noreply.github.com>
2020-09-09 19:53:59 +0545

    Add files via upload

---
+++ Exercise7.py
@@ -0,0 +1,53 @@
+#Healthy programmer
+#9am-5pm
+
+# water - water.mp3 (3.5 litres) - Drank - log-Every 40 minutes
+# Eyes- eyes.mp3 - every 30 min - ExDone - log
+# physical activity- physical.mp3 - every 45 min -Exdone-log
+
+# Pygame module to play audio from pygame import mixer
+from pygame import mixer
+from datetime import datetime
+from time import time
+
+def audioloop(file, stopper):
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play()
+    while True:
+        a = input()
+        if a == stopper:
+            mixer.music.stop()
+            break
+def log(msg):
+    with open("mylogs.txt","a") as f:
+        f.write(f"{msg} {datetime.now()}\n")
+
+if __name__ == '__main__':
+    init_water = time()
+    init_eyes = time()
+    init_exerxise = time()
+    watersec = 45
+    eyesec = 30
+    physec = 50
+
+    while True:
+        if time() - init_water > watersec:
+            print("Time for drinking water.Enter drank to stop the alarm\n")
+            audioloop("water.mp3", "drank")
+            init_water = time()
+            log("Water drank at")
+
+        if time() - init_eyes > eyesec:
+            print("Time to do eye exercise.Enter done to stop the alarm\n")
+            audioloop("eyes.mp3", "done")
+            init_eyes = time()
+            log("Eye exercise done at")
+
+        if time() - init_exerxise > physec:
+            print("Time to do physical exercise.Enter exdone to stop the alarm\n")
+            audioloop("water.mp3", "exdone")
+            init_exerxise = time()
+            log("Physical exercise done  at")
+
+
